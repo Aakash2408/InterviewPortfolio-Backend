5348de54214b8c142e59e33a9c7cfdf82013d13c
Aakash Sangwan <aakashsangwan024@gmail.com>
2021-06-20 16:46:00 +0800

    Port

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const app = express();
 const cors = require('cors');
 
 const dbService = require('./dbService');
+const { request, response } = require('express');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended : false }));
-
+app.get("/interviews",(request,response)=>{
+    response.json("Hello JSON")
+})
 
 // To get all users data 
 app.get('/getAll', (request, response) => {
@@ -69,4 +72,4 @@ app.patch('/updateInterview', (request, response) => {
     .catch(err => console.log(err));
 });
 
-app.listen(procces.env.port||5000, () => console.log('app is running'));
+app.listen(procces.env.PORT||5000, () => console.log('app is running'));
